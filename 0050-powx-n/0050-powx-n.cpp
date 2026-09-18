f8062996bfb6c35441f6260ec47c73fcfb431fc1
NirobDas29 <nirobdasdas29@gmail.com>
2026-09-18 16:22:39 +0600

    Time: 0 ms (100%), Space: 8.6 MB (71.89%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long exp =n;
+        if(exp<0)
+        {
+            x = 1/x;
+            exp =-exp;
+        }
+        return power(x,exp,1);
+        
+    }
+    double power(double x,long long n,double ans)
+    {
+        if (n==0)
+        {
+            return ans;
+        }
+        if(n%2==1)
+        {
+            ans = ans*x;
+            
+        }
+       return  power(x*x,n/2,ans);
+
+    }
+};
