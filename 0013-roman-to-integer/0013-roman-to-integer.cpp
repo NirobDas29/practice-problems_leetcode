f1b21a70ee4a01ec900a11c0632b7acfea4d817e
NirobDas29 <nirobdasdas29@gmail.com>
2026-09-26 13:52:18 +0600

    Time: 3 ms (74.48%), Space: 9.9 MB (99.17%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int total = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            int curr, next = 0;
+
+            switch (s[i]) {
+                case 'I': curr = 1; break;
+                case 'V': curr = 5; break;
+                case 'X': curr = 10; break;
+                case 'L': curr = 50; break;
+                case 'C': curr = 100; break;
+                case 'D': curr = 500; break;
+                case 'M': curr = 1000; break;
+            }
+
+            if (i + 1 < n) {
+                switch (s[i + 1]) {
+                    case 'I': next = 1; break;
+                    case 'V': next = 5; break;
+                    case 'X': next = 10; break;
+                    case 'L': next = 50; break;
+                    case 'C': next = 100; break;
+                    case 'D': next = 500; break;
+                    case 'M': next = 1000; break;
+                }
+            }
+
+            if (curr < next)
+                total -= curr;
+            else
+                total += curr;
+        }
+
+        return total;
+    }
+};
